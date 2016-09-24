4c875b95a8f0585d0ba3eeadab3b1f467a67e6d4
Karl <khughes.me@gmail.com>
2016-09-23 19:18:32 -0500

    Updating env configs

--- config/database.php
+++ config/database.php
@@ -76,7 +76,7 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
         ],
 
     ],
