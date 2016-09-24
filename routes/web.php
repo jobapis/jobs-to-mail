de3f216040d9b3bf25ae7dee5ad62ac6ae85645e
Karl <khughes.me@gmail.com>
2016-09-23 19:11:29 -0500

    Testing line

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,11 @@
 
 Route::get('/', 'UsersController@index');
 
-Route::group(['prefix' => 'users'], function () {\
+Route::group(['prefix' => 'users'], function () {
+
+    // Get all users in array - for testing
+    Route::get('/', 'UsersController@all');
+
     // Create new user
     Route::post('/', 'UsersController@create');
 
