d04a3cb5f63e2d30d96099750ce256cac4216ac5
Karl <khughes.me@gmail.com>
2016-09-25 04:36:58 -0500

    Trying travis coverage

--- tests/unit/Notifications/JobsCollectedTest.php
+++ tests/unit/Notifications/JobsCollectedTest.php
@@ -10,8 +10,9 @@ class JobsCollectedTest extends TestCase
     public function setUp()
     {
         parent::setUp();
+        $this->job = m::mock('JobApis\Jobs\Client\Job');
         $this->jobs = [
-            0 => m::mock('JobApis\Jobs\Client\Job'),
+            0 => $this->job,
         ];
         $this->notification = new JobsCollected($this->jobs);
     }
@@ -23,6 +24,7 @@ class JobsCollectedTest extends TestCase
     }
 
     /*
+    */
     public function testItConvertsNotificationToMail()
     {
         $user = m::mock('JobApis\JobsToMail\Models\User');
@@ -30,10 +32,21 @@ class JobsCollectedTest extends TestCase
         $user->shouldReceive('getAttribute')
             ->with('id')
             ->andReturn(uniqid());
+        $this->job->shouldReceive('getTitle')
+            ->once()
+            ->andReturn(uniqid());
+        $this->job->shouldReceive('getCompanyName')
+            ->once()
+            ->andReturn(null);
+        $this->job->shouldReceive('getLocation')
+            ->once()
+            ->andReturn(null);
+        $this->job->shouldReceive('getUrl')
+            ->once()
+            ->andReturn(null);
 
         $results = $this->notification->toMail($user);
 
         $this->assertEquals(JobMailMessage::class, get_class($results));
     }
-    */
 }
