d1ea3e213a2e54cf5e52e6f7806bf8dfbbb2b010
Karl <khughes.me@gmail.com>
2016-09-25 09:23:37 -0500

    Adding configs for all providers

--- config/jobboards.php
+++ config/jobboards.php
@@ -1,19 +1,61 @@
 <?php
 
-return [
+/**
+ * Makes a best guess at the server's IP defaulting to localhost.
+ */
+if (isset($_SERVER['REMOTE_ADDR'])) {
+    $currentIp = $_SERVER['REMOTE_ADDR'];
+} elseif (isset($_SERVER["HTTP_X_FORWARDED_FOR"])) {
+    $ipAddresses = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+    $currentIp = trim(end($ipAddresses));
+} else {
+    $currentIp = 'localhost';
+}
+
+/**
+ * Makes a best guess at the user agent making this request
+ */
+$userAgent = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] :
+    'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36';
+
+/**
+ * Default job boards that require no API key permissions
+ */
+$jobboards = [
     'Careercast' => [],
     'Dice' => [],
     'Github' => [],
     'Govt' => [],
-    /*
-    'Careerbuilder' => [
-        'DeveloperKey' => env("CAREERBUILDER_KEY"),
-    ],
-    'Indeed' => [
-        'publisher' => env("INDEED_KEY"),
-    ],
-    'Usajobs' => [
-        'AuthorizationKey' => env("USAJOBS_KEY"),
-    ],
-    */
 ];
+
+/**
+ * Careerbuilder
+ * http://developer.careerbuilder.com/
+ */
+if (env("CAREERBUILDER_KEY")) {
+    $jobboards['Careerbuilder'] = [
+        'DeveloperKey' => env("CAREERBUILDER_KEY"),
+    ];
+}
+/**
+ * Indeed
+ * http://www.indeed.com/publisher
+ */
+if (env("INDEED_KEY")) {
+    $jobboards['Indeed'] = [
+        'publisher' => env("INDEED_KEY"),
+        'userip' => $currentIp,
+        'useragent' => $userAgent,
+    ];
+}
+/**
+ * USAJOBS
+ * https://developer.usajobs.gov/Search-API/Overview
+ */
+if (env("USAJOBS_KEY")) {
+    $jobboards['Usajobs'] = [
+        'AuthorizationKey' => env("USAJOBS_KEY"),
+    ];
+}
+
+return $jobboards;
