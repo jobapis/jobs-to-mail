e7d87adfd242b25709481d8f04e5f4894b591ed6
Karl Hughes <khughes.me@gmail.com>
2016-10-27 17:22:38 -0500

    Adding filters

---
+++ app/Filters/RecruiterFilter.php
@@ -0,0 +1,35 @@
+<?php namespace JobApis\JobsToMail\Filters;
+
+use JobApis\JobsToMail\Models\Recruiter;
+use JobApis\JobsToMail\Models\Search;
+
+class RecruiterFilter
+{
+    public function __construct(Recruiter $recruiter)
+    {
+        $this->recruiter = $recruiter;
+    }
+
+    /**
+     * Filters out jobs from recruiting companies if the user's prefers it.
+     *
+     * @return array
+     */
+    public function filterRecruiterJobs(array $jobs, Search $search)
+    {
+        // Make sure this search wants to filter recruiters
+        if ($search->no_recruiters === true) {
+            return array_filter($jobs, function ($job) {
+                // Make sure this job has a company
+                if (isset($job->company)) {
+                    // Make sure this company is not a recruiter
+                    $recruiter = $this->recruiter->whereNameLike($job->company)->first();
+                    if ($recruiter) {
+                        return false;
+                    }
+                }
+                return true;
+            });
+        }
+    }
+}
--- app/Jobs/SearchAndNotifyUser.php
+++ app/Jobs/SearchAndNotifyUser.php
@@ -5,8 +5,10 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Support\Facades\Log;
-use JobApis\Jobs\Client\Collection;
 use JobApis\Jobs\Client\JobsMulti;
+use JobApis\JobsToMail\Filters\CollectionFilter;
+use JobApis\JobsToMail\Filters\JobFilter;
+use JobApis\JobsToMail\Filters\RecruiterFilter;
 use JobApis\JobsToMail\Models\Recruiter;
 use JobApis\JobsToMail\Models\Search;
 use JobApis\JobsToMail\Notifications\JobsCollected;
@@ -60,119 +62,34 @@ class SearchAndNotifyUser implements ShouldQueue
      *
      * @return array
      */
-    public function handle(JobsMulti $jobsClient, Recruiter $recruiter)
-    {
-        // Going to use the recruiter model
-        $this->recruiter = $recruiter;
-
+    public function handle(
+        JobsMulti $jobsClient,
+        CollectionFilter $collectionFilter,
+        JobFilter $jobFilter,
+        RecruiterFilter $recruiterFilter
+    ) {
         // Collect jobs based on the Search keyword and location
         $jobsByProvider = $jobsClient->setKeyword($this->search->keyword)
             ->setLocation($this->search->location)
             ->setPage(1, self::MAX_JOBS_FROM_PROVIDER)
             ->getAllJobs();
 
-        // Get jobs in one array
-        $this->jobs = $this->getJobsFromCollections($jobsByProvider);
+        // Get jobs Array from array of Collections
+        $jobs = $collectionFilter->getJobsFromCollections($jobsByProvider, self::MAX_JOBS_FROM_PROVIDER);
 
         // Sort the jobs
-        $this->sortJobs();
+        $jobs = $jobFilter->sort($jobs, self::MAX_DAYS_OLD, self::MAX_JOBS);
 
         // Filter jobs from recruiters
-        $this->filterRecruiterJobs();
+        $jobs = $recruiterFilter->filterRecruiterJobs($jobs, $this->search);
+        dd($jobs);
 
         // Trigger notification to user
-        if ($this->jobs) {
-            $this->search->user->notify(new JobsCollected($this->jobs, $this->search));
+        if ($jobs) {
+            $this->search->user->notify(new JobsCollected($jobs, $this->search));
         } else {
             Log::info("No jobs found for search {$this->search->id}");
         }
-        return $this->jobs;
-    }
-
-    /**
-     * Convert the array of collections to one large array
-     *
-     * @param array $collectionsArray
-     *
-     * @return array
-     */
-    protected function getJobsFromCollections($collectionsArray = [])
-    {
-        $jobs = [];
-        array_walk_recursive(
-            $collectionsArray,
-            function (Collection $collection) use (&$jobs) {
-                $this->logErrorsFromCollection($collection);
-                $jobListings = array_slice(
-                    $collection->all(),
-                    0,
-                    self::MAX_JOBS_FROM_PROVIDER
-                );
-                foreach ($jobListings as $jobListing) {
-                    $jobs[] = $jobListing;
-                }
-            }
-        );
         return $jobs;
-    }
-
-    /**
-     * Filters out jobs from recruiting companies if the user's prefers it.
-     *
-     * @return void
-     */
-    protected function filterRecruiterJobs()
-    {
-        // Make sure this search wants to filter recruiters
-        if ($this->search->no_recruiters === true) {
-            $this->jobs = array_filter($this->jobs, function ($job) {
-                // Make sure this job has a company
-                if (isset($job->company)) {
-                    // Make sure this company is not a recruiter
-                    $recruiter = $this->recruiter->whereNameLike($job->company)->first();
-                    if ($recruiter) {
-                        return false;
-                    }
-                }
-                return true;
-            });
-        }
-    }
-
-    /**
-     * Logs all the errors attached to a collection
-     *
-     * @param array $jobsByProvider
-     *
-     * @return void
-     */
-    protected function logErrorsFromCollection(Collection $collection)
-    {
-        if ($collection->getErrors()) {
-            foreach ($collection->getErrors() as $error) {
-                Log::error($error);
-            }
-        }
-    }
-
-    /**
-     * Sort jobs by date posted, desc
-     *
-     * @param array $jobs
-     *
-     * @return array
-     */
-    protected function sortJobs($jobs = [])
-    {
-        // Sort by date
-        usort($jobs, function ($item1, $item2) {
-            return $item2->datePosted <=> $item1->datePosted;
-        });
-        // Filter any older than max age
-        $jobs = array_filter($jobs, function ($job) {
-            return $job->datePosted > new \DateTime(self::MAX_DAYS_OLD.' days ago');
-        });
-        // Truncate to the max number of results
-        return array_slice($jobs, 0, self::MAX_JOBS);
     }
 }
