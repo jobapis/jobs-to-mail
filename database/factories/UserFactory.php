f6ffe4d484286c2327f7a576fdc992c5a6ac09b7
Karl Hughes <khughes.me@gmail.com>
2016-11-01 18:32:52 -0500

    Seeding premium users

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -10,11 +10,17 @@ $factory->define(\JobApis\JobsToMail\Models\User::class, function (Faker\Generat
     return [
         'email' => $faker->safeEmail(),
         'confirmed_at' => null,
+        'tier' => config('app.user_tiers.free'),
     ];
 });
 $factory->state(\JobApis\JobsToMail\Models\User::class, 'active', function (Faker\Generator $faker) {
     return [
         'confirmed_at' => $faker->dateTimeThisYear(),
+    ];
+});
+$factory->state(\JobApis\JobsToMail\Models\User::class, 'premium', function (Faker\Generator $faker) {
+    return [
+        'tier' => config('app.user_tiers.premium'),
     ];
 });
 $factory->state(\JobApis\JobsToMail\Models\User::class, 'deleted', function (Faker\Generator $faker) {
