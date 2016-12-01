7970ba71916c2387c6a5d4254d762bc864a97975
Karl Hughes <khughes.me@gmail.com>
2016-12-01 13:03:10 -0600

    More text updates

--- resources/views/users/premium.blade.php
+++ resources/views/users/premium.blade.php
@@ -6,7 +6,7 @@
 
 <div class="row premium-title">
     <div class="col-lg-6 offset-lg-3 col-sm-8 offset-sm-2">
-        <h1 class="page-header">{{ config('app.name') }}</h1>
+        <h1 class="page-header">{{ config('app.name') }} Premium</h1>
         <p class="lead">Job board data for HR professionals</p>
 
         @include('layouts.flash-messages')
@@ -33,7 +33,7 @@
     </div>
 
     <div class="col-md-6">
-        <h2 class="text-xs-center">Activate Premium today!</h2>
+        <h2 class="text-xs-center">Sign up for Premium today</h2>
         <form method="POST" action="/users/premium">
             {{ csrf_field() }}
             <div class="form-group">
@@ -53,8 +53,10 @@
                 <label for="location">Location</label>
                 <input type="text" name="location" class="form-control" placeholder="Chicago, IL" required/>
             </div>
-
-            @include('users.components.submit')
+            <div class="form-group">
+                <input type="submit" value="Continue" class="form-control btn btn-success btn-lg"/>
+                <small><a href="/terms">Terms/Privacy Policy</a></small>
+            </div>
 
         </form>
     </div>
@@ -68,11 +70,11 @@
         <div class="row premium-info">
             <div class="col-xs-8">
                 <h3>Spot hiring trends</h3>
-                <p>Find out who's hiring in your area.</p>
+                <p>In your area or across the country.</p>
                 <ul>
-                    <li>Keep an eye out for competition.</li>
-                    <li>Know which employers have been running job listings.</li>
-                    <li>Look out for job listings that have been open a long time.</li>
+                    <li>Find out if certain jobs are being posted more often.</li>
+                    <li>Build a list of companies that hire similar employees.</li>
+                    <li>Prepare for changes in the workforce and labor market.</li>
                 </ul>
             </div>
 
@@ -89,10 +91,10 @@
 
             <div class="col-xs-8 offset-xs-1">
                 <h3>Find new clients</h3>
-                <p>We know who's hiring and they're your next potential client.</p>
+                <p>Employers could be your next client.</p>
                 <ul>
-                    <li>Download the job listings in your area.</li>
-                    <li>Filter out recruiting firms.</li>
+                    <li>Leads for independent recruiters and consultants.</li>
+                    <li>Reach out with more data at your disposal.</li>
                     <li>Keep an eye on <a href="#provider-logos" class="font-weight-bold">{{ count(config('jobboards')) }} different job boards</a> at once.</li>
                 </ul>
             </div>
@@ -102,11 +104,11 @@
         <div class="row premium-info">
             <div class="col-xs-8">
                 <h3>Learn the hiring landscape</h3>
-                <p>in your area or a new location.</p>
+                <p>In your area or a new location.</p>
                 <ul>
-                    <li>Download the job listings in your area.</li>
-                    <li>Filter out recruiting firms.</li>
-                    <li>Keep an eye on <a href="#provider-logos" class="font-weight-bold">{{ count(config('jobboards')) }} different job boards</a> at once.</li>
+                    <li>Keep an eye out for your hiring competition.</li>
+                    <li>Know which employers have been running job listings.</li>
+                    <li>Find out which job listings have been open a long time.</li>
                 </ul>
             </div>
 
