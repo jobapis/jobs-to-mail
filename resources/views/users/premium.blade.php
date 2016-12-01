1aaa9bb74b4875a4a0c50768607a98aa2d84e133
Karl Hughes <khughes.me@gmail.com>
2016-12-01 13:30:58 -0600

    Minor tweaks to template

--- resources/views/users/premium.blade.php
+++ resources/views/users/premium.blade.php
@@ -4,20 +4,11 @@
 
 @section('content')
 
-<div class="row premium-title">
-    <div class="col-lg-6 offset-lg-3 col-sm-8 offset-sm-2">
-        <h1 class="page-header">{{ config('app.name') }} Premium</h1>
-        <p class="lead">Job board data for HR professionals</p>
-
-        @include('layouts.flash-messages')
-    </div>
-</div>
-<br>
-<br>
+@include('users/components/premium-title')
 
 <div class="row premium-signup">
     <div class="col-md-6">
-        <h2 class="text-xs-center">Premium hiring data</h2>
+        <h2 class="text-xs-center">Job board data for HR professionals</h2>
         <p>JobsToMail Premium gives human resources teams, hiring managers, recruiters, and staffing professionals unrestricted access to hiring data from job boards across the web.</p>
         <p>Premium users get:</p>
         <ul>
@@ -29,7 +20,6 @@
             <li>Listings collected from <strong>many job boards and aggregators</strong>.</li>
             <li>Up to <strong>10 search terms</strong> and locations per account.</li>
         </ul>
-        <br>
     </div>
 
     <div class="col-md-6">
@@ -62,64 +52,16 @@
     </div>
 </div>
 
-<br><br>
-
-<div class="row">
-    <div class="col-md-8 offset-md-2">
-
-        <div class="row premium-info">
-            <div class="col-xs-8">
-                <h3>Spot hiring trends</h3>
-                <p>In your area or across the country.</p>
-                <ul>
-                    <li>Find out if certain jobs are being posted more often.</li>
-                    <li>Build a list of companies that hire similar employees.</li>
-                    <li>Prepare for changes in the workforce and labor market.</li>
-                </ul>
-            </div>
-
-            <div class="col-xs-3 offset-xs-1">
-                <img src="/img/graph.svg" class="img-fluid" alt="Hiring trends">
-            </div>
-        </div>
-        <br><br>
-
-        <div class="row premium-info">
-            <div class="col-xs-3">
-                <img src="/img/visible.svg" class="img-fluid" alt="New clients">
-            </div>
-
-            <div class="col-xs-8 offset-xs-1">
-                <h3>Find new clients</h3>
-                <p>Employers could be your next client.</p>
-                <ul>
-                    <li>Leads for independent recruiters and consultants.</li>
-                    <li>Reach out with more data at your disposal.</li>
-                    <li>Keep an eye on <a href="#provider-logos" class="font-weight-bold">{{ count(config('jobboards')) }} different job boards</a> at once.</li>
-                </ul>
-            </div>
-        </div>
-        <br><br>
-
-        <div class="row premium-info">
-            <div class="col-xs-8">
-                <h3>Learn the hiring landscape</h3>
-                <p>In your area or a new location.</p>
-                <ul>
-                    <li>Keep an eye out for your hiring competition.</li>
-                    <li>Know which employers have been running job listings.</li>
-                    <li>Find out which job listings have been open a long time.</li>
-                </ul>
-            </div>
-
-            <div class="col-xs-3 offset-xs-1">
-                <img src="/img/site-map.svg" class="img-fluid" alt="Hiring map">
-            </div>
-        </div>
-        <br><br>
-
+<div class="row premium-signup">
+    <div class="col-md-12">
+        <p class="h2 text-xs-center">All this for only</p>
+        <p class="h1 text-xs-center">
+            <span class="tag tag-success">$19.99</span><small class="text-muted"> per month</small>
+        </p>
     </div>
 </div>
+
+@include('users/components/premium-info')
 
 @include('layouts.providers')
 
